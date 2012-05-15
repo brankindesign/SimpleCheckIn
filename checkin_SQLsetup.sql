bc3a8b519c0084570c6c49b4b24f0df01c0c0e69
Bryan Rankin <brankin.design@gmail.com>
2012-05-15 15:19:25 -0400

    Updated SQL structure

--- checkin_SQLsetup.sql
+++ checkin_SQLsetup.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 01, 2012 at 02:20 PM
+-- Generation Time: May 15, 2012 at 03:16 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -26,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `attendance`
 --
 
-DROP TABLE IF EXISTS `attendance`;
 CREATE TABLE IF NOT EXISTS `attendance` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `child` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -38,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   `date_out` varchar(25) NOT NULL,
   `time_out` varchar(25) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=55 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=71 ;
 
 -- --------------------------------------------------------
 
@@ -46,7 +45,6 @@ CREATE TABLE IF NOT EXISTS `attendance` (
 -- Table structure for table `child`
 --
 
-DROP TABLE IF EXISTS `child`;
 CREATE TABLE IF NOT EXISTS `child` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -63,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `child` (
   `guardian4` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
 
 -- --------------------------------------------------------
 
@@ -71,13 +69,13 @@ CREATE TABLE IF NOT EXISTS `child` (
 -- Table structure for table `guardian`
 --
 
-DROP TABLE IF EXISTS `guardian`;
 CREATE TABLE IF NOT EXISTS `guardian` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `active` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
   `phone` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
-  `text` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
+  `sms` varchar(3) COLLATE utf8_unicode_ci NOT NULL,
   `carrier` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `child1` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -85,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `guardian` (
   `child3` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `child4` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=15 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
